356a53e2a87be4a81428095984f8d373cc227926
Rivers450 <55551837+Rivers450@users.noreply.github.com>
2023-04-24 16:05:13 -0400

    fixed an error where it was trying to reference something I don't have

--- models/RSVP.js
+++ models/RSVP.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const rsvpSchema = new Schema(
     {
         status: { type: String, enum: ['Yes', 'No', 'Maybe'], required: [true, "RSVPing is required"] },
-        host: { type: Schema.Types.ObjectId, ref: 'User', required: [true, "RSVP is missing attendee argument."] },
-        event: { type: Schema.Types.ObjectId, ref: 'Meetup', required: [true, "RSVP is missing event argument."] }
+        host: { type: Schema.Types.ObjectId, required: [true, "RSVP is missing attendee argument."] },
+        event: { type: Schema.Types.ObjectId, required: [true, "RSVP is missing event argument."] }
     }
 )
 
-module.exports = mongoose.model('Rsvp', rsvpSchema);
+module.exports = mongoose.model('rsvp', rsvpSchema);
