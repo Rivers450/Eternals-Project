3635ba84f2004f9b7e01f90594fc7a14ea3c9e98
Rivers450 <55551837+Rivers450@users.noreply.github.com>
2023-05-01 15:16:54 -0400

    added better formatting to the auth.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,16 @@ const Event = require('../models/eventModel');
 exports.isGuest = (req, res, next) => {
     if (!req.session.user) {
         return next();
-    }
-    else {
-        req.flash("error", "You're already logged in.");
+    } else {
+        req.flash("error", "You have already logged in.");
         return res.redirect("/users/profile");
     }
 }
 exports.isLoggedIn = (req, res, next) => {
     if (req.session.user) {
         return next();
-    }
-    else {
-        req.flash("error", "You're not logged in.");
+    } else {
+        req.flash("error", "You are currently not logged in.");
         return res.redirect("/users/login");
     }
 }
@@ -25,15 +23,13 @@ exports.isHost = (req, res, next) => {
             if (event) {
                 if (event.host == req.session.user) {
                     return next();
-                }
-                else {
-                    let err = new Error("Unauthorized to access the resource.");
+                } else {
+                    let err = new Error("You are unauthorized to access the current resources.");
                     err.status = 401;
                     return next(err);
                 }
-            }
-            else {
-                let err = new Error('Cannot find a event with id.');
+            } else {
+                let err = new Error('Cannot find the event with id that is provided.');
                 err.status = 404;
                 next(err);
             }
