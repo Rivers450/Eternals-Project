58caf01e2629b35f7f7b1c3f081af38216418244
Rivers Snyder <rsnyde18@uncc.edu>
2023-05-01 16:59:35 -0400

    Removed isGuest, which was causing an error with the validation

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const controller = require('../controllers/eventController');
 const { fileUpload } = require('../middleware/fileUpload');
-const { isLoggedIn, isHost, isGuest } = require('../middleware/auth');
+const { isLoggedIn, isHost } = require('../middleware/auth');
 const { validateId, validateEvent, validateResult, getDateInputs, validateRSVP } = require('../middleware/validator');
 
 router.get('/', controller.index);
@@ -13,6 +13,6 @@ router.get('/:id', validateId, controller.show);
 router.get('/:id/edit', validateId, isLoggedIn, isHost, controller.edit);
 router.put('/:id', validateId, isLoggedIn, isHost, fileUpload, getDateInputs, validateEvent, validateResult, controller.update);
 router.delete('/:id', validateId, isLoggedIn, isHost, controller.delete);
-router.post('/:id/rsvp', validateId, isLoggedIn, isGuest, validateRSVP, validateResult, controller.rsvp)
+router.post('/:id/rsvp', validateId, isLoggedIn, validateRSVP, validateResult, controller.rsvp)
 
 module.exports = router;
