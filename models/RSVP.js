924b1fdd0e04065912aca900f28cb55539bb48e2
Rivers450 <55551837+Rivers450@users.noreply.github.com>
2023-05-01 15:12:11 -0400

    formatting the RSVP model

--- models/RSVP.js
+++ models/RSVP.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const rsvpSchema = new Schema(
-    {
-        status: { type: String, enum: ['Yes', 'No', 'Maybe'], required: [true, "RSVPing is required"] },
-        host: { type: Schema.Types.ObjectId,ref: 'User', required: [true, "RSVP is missing attendee argument."] },
-        event: { type: Schema.Types.ObjectId, ref: 'Event',required: [true, "RSVP is missing event argument."] }
-    }
-)
+//creating a new schema
+const rsvpSchema = new Schema({
+    status: { type: String, enum: ['Yes', 'No', 'Maybe'], required: [true, "The status of the RSVP is required"] },
+    host: { type: Schema.Types.ObjectId, ref: 'User', required: [true, "The RSVP status is missing the host argument."] },
+    event: { type: Schema.Types.ObjectId, ref: 'Event', required: [true, "The RSVP status is missing the event argument."] }
+})
 
 module.exports = mongoose.model('rsvp', rsvpSchema);
